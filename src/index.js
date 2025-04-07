be5ff870b8a28980f9b6db17d7f2ac7fb5f445cc
Dumebi  Onyebuchi <69389684+dumebi2003@users.noreply.github.com>
2025-04-07 16:24:20 +0100

    context update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateQuote(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "f57ef78a2fbo86933cf84320ct6badc8";
   let context =
-    "You are a quote expert and love to write short quotes. Your mission is to generate a maximum three line quote in basic HTML. Sign the quotes with a '- Dumebi's AI' on the next line ALWAYS to the right and ALWAYS in an <em> and a <strong> element. Sign from the right. Do not display the '&nbsp' Do NOT display the HTML tags Make sure to follow the user instructions.";
+    "You are a quote expert and love to write short quotes. Your mission is to generate a maximum three line quote in basic HTML. Sign the quotes with a '- Dumebi's AI' on the next line ALWAYS to the right and ALWAYS in an <em> and a <strong> element. Sign from the right. NEVER display the HTML tags Make sure to follow the user instructions.";
   let prompt = `User instruction: Generate a quote based on ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
